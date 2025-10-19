5e72c42b00b0797d3e0899c381a28c463e8cf4d1
AnthonyGrano <ant.grano@icloud.com>
2025-10-19 16:10:13 -0400

    GPIO Setup and SPI intilization

--- STM32F091RC/Bench/Test_Bench_1/Core/Src/main.c
+++ STM32F091RC/Bench/Test_Bench_1/Core/Src/main.c
@@ -1,8 +1,6 @@
 // Test Bench SPI communication with Slave Devices
 
-#define debug
-
-
+//#define debug
 
 #include "stm32f0xx.h"
 #include <stdint.h>
@@ -32,35 +30,61 @@ int main(void)
 
 void gpioInit(void)
 {
-    // Enable GPIOA clock
-    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
-
+    // Enable GPIO B clock
+    RCC->AHBENR |= (1u << 18); // Enable GPIOB clock
+    RCC->APB2ENR |= (1u << 12); // Enable SPI1 clock
 }
 
 void pinInit(void)
 {
-    // Configure PA5 (SCK), PA6 (MISO), PA7 (MOSI) as Alternate Function
-    GPIOA->MODER &= ~(3 << (5 * 2) | 3 << (6 * 2) | 3 << (7 * 2));
-    GPIOA->MODER |= (2 << (5 * 2) | 2 << (6 * 2) | 2 << (7 * 2));
+    // Set PB3(SCK), PB4(MISO), PB5(MOSI) to Alternate Function mode
+    GPIOB->MODER &= ~(3u << (3 * 2) | 3u << (4 * 2) | 3u << (5 * 2)); // Clear mode bits for PB3, PB4, PB5
+    GPIOB->MODER |= (2u << (3 * 2) | 2u << (4 * 2) | 2u << (5 * 2));  // Set to Alternate Function mode
+    // Set PB6 (NSS) as output
+    GPIOB->MODER &= ~(3u << (6 * 2)); // Clear mode bits for PB6
+    GPIOB->MODER |= (1u << (6 * 2));  // Set to General Purpose Output mode
 
 }
 
 void setAF(void)
 {
-    // Set Alternate Function 0 for PA5, PA6, PA7
-    GPIOA->AFR[0] &= ~(0xF << (5 * 4) | 0xF << (6 * 4) | 0xF << (7 * 4));
-    GPIOA->AFR[0] |= (0 << (5 * 4) | 0 << (6 * 4) | 0 << (7 * 4));
+    // Set Alternate Function 0 (AF0) for PB3(SCK), PB4(MISO), PB5(MOSI)    
+    //redo 
+    GPIOB->AFR[0] &= ~(15u << (3 * 4) | 15u << (4 * 4) | 15u << (5 * 4)); // Clear AFRL bits for PB3, PB4, PB5
+    GPIOB->AFR[0] |= (0u << (3 * 4) | 0u << (4 * 4) | 0u << (5 * 4));  // Set AF0 for SPI1
+
 }
 
 void spiInit(void)
 {
-    // Enable SPI1 clock
-    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
-
-    // Configure SPI1 in Master mode, CPOL=0, CPHA=0, 8-bit data frame, Baud rate = fPCLK/16
-    SPI1->CR1 = SPI_CR1_MSTR | SPI_CR1_BR_1 | SPI_CR1_BR_0 | SPI_CR1_SSM | SPI_CR1_SSI;
-
-    // Enable SPI1
-    SPI1->CR1 |= SPI_CR1_SPE;
+    // Configure SPI1
+    SPI1->CR1 |= (0u << 15) // MIDIMODE 0 full duplex 1 half duplex
+               | (0u << 14) // Bidirectional mode disabled 0 recive only 1 transmit only
+               | (0u << 13) // CRC Calcualtion disabled 0 disabled 1 enabled
+               | (0u << 12) // CRCNEXT 0 next transmit vlaue is from the Tx buffer 1 next transmit value is from the CRC register
+               | (0u << 11) // CRC Length 0 8-bit 1 16-bit
+               | (0u << 10) // RX Only 0 FULL DUPLEX 1 RX ONLY
+               | (1u << 9)  // SSM Software slave management enabled
+               | (1u << 8)  // SSI interal Slave Select
+               | (0u << 7)  // LSB FIRST 0 MSB 1 LSB
+               | (1u << 6)  // SPI enabled
+               | (4u << 3)  // Baud rate control: fPCLK/X
+               | (1u << 2)  // Master Selection
+               | (1u << 1); // Clock Polarity
+               | (1u << 0); // Clock Phase
+    
+    SPI1->CR2 |= (0u << 14) // LDMA_TX 0 number of data to transfer is even 1 odd
+               | (0u << 13) // LEMA)RX 0 number of data to transfer is even 1 odd
+               | (0u << 12) // FRXTH 0 RXNE event is generated if the FIFO level is greater than or equal to 1/2 1 RXNE event is generated if the FIFO level is greater than or equal to 1/4
+               | (0u << 8) // DATA Size PG 817
+               | (0u << 7) // TXEIE 0 TXE interupt Masked 1 TXE interrupt not masked
+               | (0u << 6) // RXNEIE 0 RXNE interrupt Masked 1 RXNE interrupt not masked
+               | (0u << 5) // ERRIE 0 Error interrupt Masked 1 Error interrupt is enabled
+               | (0u << 4) // FRF 0 Motorola 1 TI
+               | (0u << 3) // NSSP 0 no NSS Pulse 1 NSS pulse generated
+               | (0u << 2) // SSOE 0  SS output disabled 1 SS output enabled
+               | (0u << 1) // TXDMAEN 0 TX DMA disabled 1 TX DMA enabled
+               | (0u << 0); // RXDMAEN 0 RX DMA disabled 1 RX DMA enabled
+   
 }
 
