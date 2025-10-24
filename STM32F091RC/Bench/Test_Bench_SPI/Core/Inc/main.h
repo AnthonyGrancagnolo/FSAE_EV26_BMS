21f9b8c4755e95c659859720b8bb019d390fc4e5
AnthonyGrancagnolo <116761946+AnthonyGrancagnolo@users.noreply.github.com>
2025-10-24 11:34:52 -0400

    L9963E library added

--- STM32F091RC/Bench/Test_Bench_SPI/Core/Inc/main.h
+++ STM32F091RC/Bench/Test_Bench_SPI/Core/Inc/main.h
@@ -27,6 +27,7 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -49,17 +50,58 @@ extern "C" {
 /* USER CODE END EM */
 
 /* Exported functions prototypes ---------------------------------------------*/
-
+void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
 
+// Functions Prototypes
+void GPIOInit(void);
+void setAF(void);
+void SPI1Init(void);
+void L9963ETXX(uint8_t TX);
+void L9963ERXX(uint8_t RX);
 
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define B1_Pin GPIO_PIN_13
+#define B1_GPIO_Port GPIOC
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin GPIO_PIN_3
+#define USART_RX_GPIO_Port GPIOA
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
+// Pin Defines
+#define SPI1_PORT GPIOA
+
+#define SPI1_CS_PORT GPIOA
+#define SPI1_CS_PIN 4
+
+#define SPI1_SCK_PORT GPIOA
+#define SPI1_SCK_PIN 5
+
+#define SPI1_MISO_PORT GPIOA
+#define SPI1_MISO_PIN 6
+
+#define SPI1_MOSI_PORT GPIOA
+#define SPI1_MOSI_PIN 7
+
+// --- DEVICE CONSTANTS ---
+#define L9963E_FRAME_BITS   20      // The L9963E uses a 20-bit frame
+#define L9963E_WRITE        0       // R/W bit for a write operation (bit 19)
+#define L9963E_READ         1       // R/W bit for a read operation (bit 19)
+
+// --- FRAME STRUCTURE MACROS (Conceptual: R/W(1)|ADDR(8)|DATA(11) ) ---
+// Note: This assumes the L9963E uses a fixed 4-bit CRC/parity within the 20-bit frame
+// or a simplified structure. The actual frame structure must be verified from the datasheet.
+#define L9963E_ADDRESS_MASK 0xFF
+#define L9963E_DATA_MASK    0x7FF   // 11 bits of data
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
--- STM32F091RC/Bench/Test_Bench_SPI/Core/Src/main.c
+++ STM32F091RC/Bench/Test_Bench_SPI/Core/Src/main.c
@@ -1,47 +1,15 @@
+
 #include "stm32f0xx.h"
 #include <stdint.h>
 #include <stdio.h>
-//Test
-// Pin Defines
-#define SPI1_PORT GPIOA
+#include "main.h"
 
-#define SPI1_CS_PORT GPIOA
-#define SPI1_CS_PIN 4
-
-#define SPI1_SCK_PORT GPIOA
-#define SPI1_SCK_PIN 5
-
-#define SPI1_MISO_PORT GPIOA
-#define SPI1_MISO_PIN 6
-
-#define SPI1_MOSI_PORT GPIOA
-#define SPI1_MOSI_PIN 7
-
-
-
-// --- DEVICE CONSTANTS ---
-#define L9963E_FRAME_BITS   20      // The L9963E uses a 20-bit frame
-#define L9963E_WRITE        0       // R/W bit for a write operation (bit 19)
-#define L9963E_READ         1       // R/W bit for a read operation (bit 19)
-
-// --- FRAME STRUCTURE MACROS (Conceptual: R/W(1)|ADDR(8)|DATA(11) ) ---
-// Note: This assumes the L9963E uses a fixed 4-bit CRC/parity within the 20-bit frame
-// or a simplified structure. The actual frame structure must be verified from the datasheet.
-#define L9963E_ADDRESS_MASK 0xFF
-#define L9963E_DATA_MASK    0x7FF   // 11 bits of data
 
 // --- STATIC STORAGE FOR THE OUT-OF-FRAME RESPONSE ---
 // The L9963E returns the answer to the N-th command in the (N+1)-th frame (out-of-frame).
 // This variable stores the response received from the *previous* SPI transaction.
 static uint32_t last_rx_frame = 0;
 
-
-// Functions Prototypes
-void GPIOInit(void);
-void setAF(void);
-void SPI1Init(void);
-void L9963ETXX(uint8_t TX);
-void L9963ERXX(uint8_t RX);
 
 int main(){
 	GPIOInit();
@@ -50,7 +18,6 @@ int main(){
 
 	while(1){
 		// TODO Send transmission
-
 
 	}
 
@@ -62,42 +29,35 @@ void GPIOInit(void){
 	RCC->AHBENR  |= RCC_AHBENR_GPIOAEN;
 
 
-	// Enable Extras
+	// Enable SPI
 	RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
-
-}
-
-void setAF(void){
-	// TODO Configure PA4 (NCS) PA5 (SCK) PA6 (MISO) and PA7 (MOSI) as GPIO alternate function
 	// Clear mode bits
 	SPI1_PORT->MODER &= ~((3 << (SPI1_CS_PIN * 2)) | (3 << (SPI1_SCK_PIN * 2)) | (3 << (SPI1_MISO_PIN * 2)) | (3 << (SPI1_MOSI_PIN * 2)));
 	 // Set to Alternate Function mode
 	SPI1_PORT->MODER |= (2 << (SPI1_CS_PIN * 2)) | (2 << (SPI1_SCK_PIN * 2)) | (2 << (SPI1_MISO_PIN * 2)) | (2 << (SPI1_MOSI_PIN * 2));
 	// Clear AFRL bits and set to AF0
 	SPI1_PORT->AFR[0] &= ~((0xF << (SPI1_CS_PIN * 4)) | (0xF << (SPI1_SCK_PIN * 4)) | (0xF << (SPI1_MISO_PIN * 4)) | (0xF << (SPI1_MOSI_PIN * 4)));
+
 }
+
 
 void SPI1Init(){
 	// TODO Set configure CR1 and CR2
 	// Configure SPI1
 	    SPI1->CR1 |= (0u << 15) // MIDIMODE 0 full duplex 1 half duplex
-	               | (0u << 14) // Bidirectional mode disabled 0 recive only 1 transmit only
-	               | (0u << 13) // CRC Calcualtion disabled 0 disabled 1 enabled
-	               | (0u << 12) // CRCNEXT 0 next transmit vlaue is from the Tx buffer 1 next transmit value is from the CRC register
-	               | (0u << 11) // CRC Length 0 8-bit 1 16-bit
 	               | (0u << 10) // RX Only 0 FULL DUPLEX 1 RX ONLY
 	               | (1u << 9)  // SSM Software slave management enabled
 	               | (1u << 8)  // SSI interal Slave Select
-	               | (0u << 7)  // LSB FIRST 0 MSB 1 LSB
-	               | (4u << 3)  // Baud rate control: fPCLK/X
+	               | (1u << 7)  // LSB FIRST 0 MSB 1 LSB
+	               | (7u << 3)  // Baud rate control: fPCLK/X
 	               | (1u << 2)  // Master Selection
-	               | (1u << 1) // Clock Polarity
+	               | (1u << 1) 	// Clock Polarity
 	               | (1u << 0); // Clock Phase
 
 	    SPI1->CR2 |= (0u << 14) // LDMA_TX 0 number of data to transfer is even 1 odd
 	               | (0u << 13) // LEMA)RX 0 number of data to transfer is even 1 odd
 	               | (0u << 12) // FRXTH 0 RXNE event is generated if the FIFO level is greater than or equal to 1/2 1 RXNE event is generated if the FIFO level is greater than or equal to 1/4
-	               | (0u << 8) // DATA Size PG 817
+	               | (7u << 8) // DATA Size PG 817
 	               | (0u << 7) // TXEIE 0 TXE interupt Masked 1 TXE interrupt not masked
 	               | (0u << 6) // RXNEIE 0 RXNE interrupt Masked 1 RXNE interrupt not masked
 	               | (0u << 5) // ERRIE 0 Error interrupt Masked 1 Error interrupt is enabled
@@ -127,7 +87,7 @@ void L9963ERXX(uint8_t RX){
 
 	// wait till buffer is empty
 	while (!(SPI1->SR & SPI_SR_RXE));
-	// TODO TX
+	// TODO RX
 
 	// wait for transmission to complete
 	while (SPI1->SR & SPI_SR_BSY);
